225aed409dc89c91603fe3a89cf6fe07abbe4ca0
Dharyansh Achlas <164983962+salchaD-27@users.noreply.github.com>
2025-01-18 22:28:14 +0530

    Add files via upload

---
+++ LeetCode/34_M_First_And_Last_Position_Of_Sorted_Array.py
@@ -0,0 +1,8 @@
+from typing import List
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        ans=[]
+        for i in range(len(nums)):
+            if nums[i]==target: ans.append(i)
+        if(len(ans)==0): return [-1, -1]
+        return [ans[0], ans[-1]]
