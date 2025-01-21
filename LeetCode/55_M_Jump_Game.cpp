d2937b5695b28748247650cb9f1e66024069bae5
Dharyansh Achlas <164983962+salchaD-27@users.noreply.github.com>
2025-01-21 21:24:55 +0530

    Add files via upload

---
+++ LeetCode/55_M_Jump_Game.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size(), farthest=0;
+        for(int i=0; i<n; i++){
+            if (i > farthest){return false;}
+            farthest = max(farthest, i + nums[i]);
+            if (farthest >= n - 1){return true;}
+        }
+        return false;
+    }
+};
