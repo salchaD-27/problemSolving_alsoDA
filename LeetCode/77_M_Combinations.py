baf53e1466d1880e515667acb7d225b6d9159caf
Dharyansh Achlas <164983962+salchaD-27@users.noreply.github.com>
2025-01-24 22:10:05 +0530

    Add files via upload

---
+++ LeetCode/77_M_Combinations.py
@@ -0,0 +1,14 @@
+from typing import List
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        def backtrack(start: int, combination: List[int]):
+            if len(combination) == k:
+                result.append(combination[:]) 
+                return
+            for i in range(start, n + 1):
+                combination.append(i) 
+                backtrack(i + 1, combination)
+                combination.pop() 
+        result = []
+        backtrack(1, [])
+        return result
