a2fbba9eb5747144df1ff486a128b403867a8801
Dharyansh Achlas <164983962+salchaD-27@users.noreply.github.com>
2025-01-31 22:01:46 +0530

    Add files via upload

---
+++ LeetCode/139_M_Word_Break.py
@@ -0,0 +1,20 @@
+# from typing import List
+# class Solution:
+#     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+#         for word in wordDict:
+#             s=s.replace(word, "")
+#         return (not s)
+
+from typing import List
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        wordSet = set(wordDict)
+        n = len(s)
+        dp = [False] * (n + 1)
+        dp[0] = True 
+        for i in range(1, n + 1):
+            for j in range(i):
+                if dp[j] and s[j:i] in wordSet:
+                    dp[i] = True
+                    break 
+        return dp[n]
