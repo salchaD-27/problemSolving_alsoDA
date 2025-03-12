7ad30998c56371720fc43fab12b4f2e29678bfd2
Dharyansh Achlas <164983962+salchaD-27@users.noreply.github.com>
2025-03-12 23:25:15 +0530

    Add files via upload

---
+++ LeetCode/406_M_Queue_Reconstruction_By_Height.py
@@ -0,0 +1,8 @@
+from typing import List
+class Solution:
+    def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
+        people.sort(key=lambda x: (-x[0], x[1]))  
+        queue = []
+        for person in people:
+            queue.insert(person[1], person)
+        return queue
