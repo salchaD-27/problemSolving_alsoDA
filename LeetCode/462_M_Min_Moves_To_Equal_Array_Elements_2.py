981628cbce65db0149bf5b529d7f8a69d0fb4c29
Dharyansh Achlas <164983962+salchaD-27@users.noreply.github.com>
2025-04-21 19:18:13 +0530

    Add files via upload

---
+++ LeetCode/462_M_Min_Moves_To_Equal_Array_Elements_2.py
@@ -0,0 +1,6 @@
+from typing import List
+class Solution:
+    def minMoves2(self, nums: List[int]) -> int:
+        nums.sort()
+        median = nums[len(nums) // 2]
+        return sum(abs(num - median) for num in nums)
