1cfff0ec9042dacecbcebad6eca8117969c071bd
Dharyansh Achlas <164983962+salchaD-27@users.noreply.github.com>
2025-04-27 20:57:42 +0530

    Add files via upload

---
+++ LeetCode/3392_E_Count_Subarrays_Of_Length_3_With_A_Condition.py
@@ -0,0 +1,7 @@
+from typing import List
+class Solution:
+    def countSubarrays(self, nums: List[int]) -> int:
+        count=0
+        for i in range(1,len(nums)-1):
+            if(2*(nums[i-1]+nums[i+1])==nums[i]): count+=1
+        return count
