c065f0b6ac2bbc06bc60a97f2a53ec85b7f3975c
Dharyansh Achlas <164983962+salchaD-27@users.noreply.github.com>
2025-05-17 17:59:00 +0530

    Add files via upload

---
+++ LeetCode/669_M_Trim_A_BST.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+struct TreeNode* trimBST(struct TreeNode* root, int low, int high) {
+    if (root == NULL) return NULL;
+    if (root->val < low) {return trimBST(root->right, low, high);}
+    if (root->val > high) {return trimBST(root->left, low, high);}
+    root->left = trimBST(root->left, low, high);
+    root->right = trimBST(root->right, low, high);
+    return root;
+}
