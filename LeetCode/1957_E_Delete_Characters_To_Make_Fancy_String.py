c79741d6524015959feb29b33f15314acadbb9c7
Dharyansh Achlas <164983962+salchaD-27@users.noreply.github.com>
2025-07-21 19:49:18 +0530

    Add files via upload

---
+++ LeetCode/1957_E_Delete_Characters_To_Make_Fancy_String.py
@@ -0,0 +1,7 @@
+class Solution:
+    def makeFancyString(self, s: str) -> str:
+        result = []
+        for char in s:
+            if len(result) >= 2 and result[-1] == result[-2] == char: continue
+            result.append(char)
+        return ''.join(result)
