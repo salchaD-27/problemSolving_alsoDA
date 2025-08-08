946a4d5caa5d80d01722d32883689282c8ad9b21
Dharyansh Achlas <164983962+salchaD-27@users.noreply.github.com>
2025-08-08 14:14:48 +0530

    Add files via upload

---
+++ LeetCode/946_M_Validate_Stack_Sequences.py
@@ -0,0 +1,10 @@
+class Solution:
+    def validateStackSequences(self, pushed: list[int], popped: list[int]) -> bool:
+        stack = []
+        j = 0
+        for x in pushed:
+            stack.append(x)
+            while stack and stack[-1] == popped[j]:
+                stack.pop()
+                j += 1
+        return not stack
