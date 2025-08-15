8c564e5e2ef46aaaf135fce24b49a8853fb5352a
Dharyansh Achlas <164983962+salchaD-27@users.noreply.github.com>
2025-08-15 23:56:02 +0530

    Delete LeetCode/966_M_Vowel_Spellchecker.py

--- LeetCode/966_M_Vowel_Spellchecker.py
+++
@@ -1,8 +0,0 @@
-import re
-from typing import List
-class Solution:
-    def spellchecker(self, wordlist: List[str], queries: List[str]) -> List[str]:
-        words = {w: w for w in wordlist}
-        cap = {w.lower(): w for w in wordlist[::-1]}
-        vowel = {re.sub("[aeiou]", '#', w.lower()): w for w in wordlist[::-1]}
-        return [words.get(w) or cap.get(w.lower()) or vowel.get(re.sub("[aeiou]", '#', w.lower()), "") for w in queries]
