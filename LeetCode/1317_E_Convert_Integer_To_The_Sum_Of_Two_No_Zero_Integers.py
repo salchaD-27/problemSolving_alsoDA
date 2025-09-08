dab3e6adce187c22892fb7a2f9288eadf09afbeb
Dharyansh Achlas <164983962+salchaD-27@users.noreply.github.com>
2025-09-09 04:37:37 +0530

    Add files via upload

---
+++ LeetCode/1317_E_Convert_Integer_To_The_Sum_Of_Two_No_Zero_Integers.py
@@ -0,0 +1,8 @@
+from typing import List
+class Solution:
+    def getNoZeroIntegers(self, n: int) -> List[int]:
+        def check(x): return '0' not in str(x)
+
+        for i in range(1, n):
+            j = n - i
+            if check(i) and check(j): return [i, j]
