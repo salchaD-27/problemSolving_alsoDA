640ded680e3c0510d73520307be26a38147e03e6
Dharyansh Achlas <164983962+salchaD-27@users.noreply.github.com>
2025-09-09 22:54:49 +0530

    Add files via upload

---
+++ LeetCode/3477_E_Fruits_Into_Baskets_2.py
@@ -0,0 +1,17 @@
+from typing import List
+class Solution:
+    def numOfUnplacedFruits(self, fruits: List[int], baskets: List[int]) -> int:
+        n = len(fruits)
+        used = [False] * n
+        unplaced = 0
+
+        for fruit in fruits:
+            placed = False
+            for i in range(n):
+                if not used[i] and baskets[i] >= fruit:
+                    used[i] = True
+                    placed = True
+                    break
+            if not placed: unplaced += 1
+
+        return unplaced
