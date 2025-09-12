35d03899d9a91aadc4a5463900e3da848b1558fd
Dharyansh Achlas <164983962+salchaD-27@users.noreply.github.com>
2025-09-12 19:51:25 +0530

    Add files via upload

---
+++ LeetCode/1023_M_Camelcase_Matching.py
@@ -0,0 +1,15 @@
+from typing import List
+from collections import Counter
+class Solution:
+    def camelMatch(self, queries: List[str], pattern: str) -> List[bool]:
+        answer = []
+        for x in queries:
+            l = r = 0
+            while l < len(pattern) and r < len(x):
+                if pattern[l] == x[r]:
+                    l += 1
+                    r += 1
+                else: r += 1
+            if l == len(pattern) and all(map(str.islower, Counter(x) - Counter(pattern))): answer.append(True)
+            else: answer.append(False)
+        return answer
