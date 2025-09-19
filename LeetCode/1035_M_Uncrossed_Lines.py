fcd644a0780bd3de3d1ca10c5ae43cb61993b911
Dharyansh Achlas <164983962+salchaD-27@users.noreply.github.com>
2025-09-19 23:31:40 +0530

    Add files via upload

---
+++ LeetCode/1035_M_Uncrossed_Lines.py
@@ -0,0 +1,8 @@
+from collections import defaultdict
+class Solution:
+    def maxUncrossedLines(self, A, B):
+        dp, m, n = defaultdict(int), len(A), len(B)
+        for i in range(m):
+            for j in range(n):
+                dp[i, j] = max(dp[i - 1, j - 1] + (A[i] == B[j]), dp[i - 1, j], dp[i, j - 1])
+        return dp[m - 1, n - 1]
