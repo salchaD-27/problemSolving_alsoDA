a55cc14010a39d54e639f92e4fa31c0fc4cf42af
Dharyansh Achlas <164983962+salchaD-27@users.noreply.github.com>
2025-09-22 12:09:21 +0530

    Add files via upload

---
+++ LeetCode/3005_E_Count_Elements_With_Maximum_Frequency.py
@@ -0,0 +1,14 @@
+from typing import List
+class Solution:
+    def maxFrequencyElements(self, nums: List[int]) -> int:
+        freq = [0] * 101
+        mx = 0
+        res = 0
+        for n in nums:
+            freq[n] += 1
+            f = freq[n]
+            if f > mx:
+                mx = f
+                res = f
+            elif f == mx: res += f
+        return res
