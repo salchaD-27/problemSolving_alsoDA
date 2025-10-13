f490d7ee6aac0ea796a71057c05a68360d115b7d
Dharyansh Achlas <164983962+salchaD-27@users.noreply.github.com>
2025-10-13 20:59:50 +0530

    Add files via upload

---
+++ LeetCode/1130_M_Minimum_Cost_Tree_From_Leaf_Values.py
@@ -0,0 +1,13 @@
+from typing import List
+class Solution:
+    def mctFromLeafValues(self, arr: List[int]) -> int:
+        stack = [float('inf')]
+        res = 0
+        for num in arr:
+            while stack and stack[-1] <= num:
+                cur = stack.pop()
+                if stack: res += cur * min(stack[-1], num)
+            stack.append(num)
+        while len(stack) > 2:
+            res += stack.pop() * stack[-1]
+        return res
