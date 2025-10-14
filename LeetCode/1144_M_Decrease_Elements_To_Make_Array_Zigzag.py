728a07faafcb85455acfe6b9ac04f1a7607e8824
Dharyansh Achlas <164983962+salchaD-27@users.noreply.github.com>
2025-10-14 21:47:10 +0530

    Add files via upload

---
+++ LeetCode/1144_M_Decrease_Elements_To_Make_Array_Zigzag.py
@@ -0,0 +1,7 @@
+class Solution:
+    def movesToMakeZigzag(self, A):
+        A = [float('inf')] + A + [float('inf')]
+        res = [0, 0]
+        for i in range(1, len(A) - 1):
+            res[i % 2] += max(0, A[i] - min(A[i - 1], A[i + 1]) + 1)
+        return min(res)
