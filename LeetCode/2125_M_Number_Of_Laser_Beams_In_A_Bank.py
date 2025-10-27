fdcdf671d0df53499c0d311a12c8ccbbf509c446
Dharyansh Achlas <164983962+salchaD-27@users.noreply.github.com>
2025-10-27 20:33:18 +0530

    Add files via upload

---
+++ LeetCode/2125_M_Number_Of_Laser_Beams_In_A_Bank.py
@@ -0,0 +1,11 @@
+from typing import List
+class Solution:
+    def numberOfBeams(self, bank: List[str]) -> int:
+        ans=0
+        prev=0
+        for row in bank:
+            dev=row.count('1')
+            if dev>0:
+                ans+=dev*prev
+                prev=dev
+        return ans
