3d2bf119d00593955def1c4f7bca6c323d13a389
Dharyansh Achlas <164983962+salchaD-27@users.noreply.github.com>
2025-11-12 20:54:53 +0530

    Add files via upload

---
+++ LeetCode/1288_M_Remove_Covered_Intervals.py
@@ -0,0 +1,11 @@
+from typing import List
+class Solution:
+    def removeCoveredIntervals(self, intervals: List[List[int]]) -> int:
+        intervals.sort(key=lambda x: (x[0], -x[1]))
+        count = 0
+        right = 0
+        for start, end in intervals:
+            if end > right:
+                count += 1
+                right = end
+        return count
