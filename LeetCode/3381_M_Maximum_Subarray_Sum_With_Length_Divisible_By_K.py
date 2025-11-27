e8ab8b15c8c4bacc192403a0383463e5f6d88624
Dharyansh Achlas <164983962+salchaD-27@users.noreply.github.com>
2025-11-27 20:04:04 +0530

    Add files via upload

---
+++ LeetCode/3381_M_Maximum_Subarray_Sum_With_Length_Divisible_By_K.py
@@ -0,0 +1,13 @@
+from typing import List
+class Solution:
+    def maxSubarraySum(self, nums: List[int], k: int) -> int:
+        inf=float('inf')
+        n=len(nums)
+        minS=[inf]*(k-1)+[0]
+        prefix, ans=0, -inf
+        for i, x in enumerate(nums):
+            prefix+=x
+            ik=i%k
+            ans=max(ans, prefix-minS[ik])
+            minS[ik]=min(prefix, minS[ik])
+        return ans
