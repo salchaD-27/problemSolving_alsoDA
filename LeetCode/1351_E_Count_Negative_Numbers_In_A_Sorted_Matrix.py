0d7fa9b6f1aa1c8ffb2f3c9133292c6512d6df38
Dharyansh Achlas <164983962+salchaD-27@users.noreply.github.com>
2025-12-28 12:21:24 +0530

    Add files via upload

---
+++ LeetCode/1351_E_Count_Negative_Numbers_In_A_Sorted_Matrix.py
@@ -0,0 +1,9 @@
+from typing import List
+class Solution:
+    def countNegatives(self, grid: List[List[int]]) -> int:
+        count=0
+        newl=[]
+        for i in grid:
+            for j in i:
+                if j<0: count+=1
+        return count
